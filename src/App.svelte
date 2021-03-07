a0d59bba6e9ded637d576414245b115472a29040
Seth Erickson <sr.erickson@gmail.com>
2021-03-06 19:45:57 -0500

    styling and new email options

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,8 @@
+<svelte:head>
+	<title>Curation Email Helper</title>
+	<html lang="en" />
+</svelte:head>
+
 <script>
 	import Select from 'svelte-select';
 	import Config from './config.json';
@@ -20,24 +25,51 @@
 			}
 		}
 	}
+	const groupBy = (item) => item.group;
 </script>
 
 <style>
+.app-container {
+	width: 750px;
+	margin: 0 auto;
+}
+.email-text{
+	border: 1px solid #d8dbdf;
+    border-radius: 3px;
+    padding: 20px;
+    margin: 10px auto;
+}
+
+.email-select {
+	--height: 40px;
+	--multiItemBorderRadius: 2px;
+	--multiClearHeight: 10px;
+	--multiClearWidth: 10px;
+	--multiClearTop: 11px;
+	--multiClearRadius: 50%;
+}
+.email-text  p {
+	margin: 0px;
+}
 </style>
 
+<div class="app-container">
+	<div class="email-select">
+		<Select items={Config.items} on:select={handleSelect} isMulti={true} {groupBy}></Select>
+	</div>
+	<div class="email-text">
+		{@html Config.preamble}
+		{#each Config.groups as group}
+			{#if selections[group.name].length > 0 }
+				<p>{@html group.preamble}</p>
+				<ul>
+					{#each selections[group.name] as i}
+						<li><b>{i.label}</b>: {@html i.value}</li>
+					{/each}
+				</ul>
+			{/if}
+		{/each}
+		{@html Config.postscript}
+	</div>
 
-<Select items={Config.items} on:select={handleSelect} isMulti=true></Select>
-<div>
-	{@html Config.preamble}
-	{#each Config.groups as group}
-		{#if selections[group.name].length > 0 }
-			<p>{@html group.preamble}</p>
-			<ul>
-				{#each selections[group.name] as i}
-					<li><b>{i.label}</b>: {@html i.value}</li>
-				{/each}
-			</ul>
-		{/if}
-	{/each}
-	{@html Config.postscript}
 </div>
